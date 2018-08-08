afad8645dd21efe8650e383687e439acc1774963
xon-xoff <dantesraven@yahoo.com>
2018-08-08 14:26:13 -0400

    intro assignment complete: pass all tests

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,34 +4,50 @@
 
 def sum arr
   return 0 if arr.empty?
-  return arr.sum
+  arr.sum
 end
 
 def max_2_sum arr
-  return 0 if arr.empty
-  return arr.max(2).sum
+  return 0 if arr.empty?
+  arr.max(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2)
+     .to_a
+     .map{ |e| e.sum }
+     .include? n
 end
 
 # Part 2
 
-def hello(name)
-  # YOUR CODE HERE
+def hello name
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return false unless s =~ /^[[:alpha:]]/
+  s !~ /\A^[AEIOU]/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless s.scan(/^\d+$/).any?
+  s.to_i(2).even?
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize isbn, price
+    raise ArgumentError, "A valid ISBN must be entered" if isbn.empty?
+    raise ArgumentError, "A valid Price must be entered" if price <= 0.0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$%.2f" % price
+  end
 end
